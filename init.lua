19a359347e62b07177d0f89f9629b5939586f561
Michael Eberhardt <github@michael-eberhardt.de>
2023-03-11 10:06:26 +0200

    initial vim script transferring to lua

---
+++ init.lua
@@ -0,0 +1,33 @@
+-- General editor settings
+vim.cmd(':set number')
+vim.cmd(':set number')
+vim.cmd(':set relativenumber')
+vim.cmd(':set autoindent')
+vim.cmd(':set tabstop=4')
+vim.cmd(':set shiftwidth=4')
+vim.cmd(':set smarttab')
+vim.cmd(':set softtabstop=4')
+vim.cmd(':set mouse=a')
+
+
+-- Plugin-Manager / Plugins
+
+local Plug = vim.fn['plug#']
+vim.call('plug#begin') -- call plug#begin()
+
+	-- more to be added here
+	Plug('https://github.com/vim-airline/vim-airline') -- Airline
+	Plug('https://github.com/preservim/nerdtree') -- NerdTree
+	Plug('https://github.com/nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'}) -- Tree-sitter
+
+vim.call('plug#end') -- call plug#end()
+
+
+-- NerdTree Configuration
+
+-- nnoremap <C-f> :NERDTreeFocus<CR>
+-- nnoremap <C-n> :NERDTree<CR>
+-- nnoremap <C-t> :NERDTreeToggle<CR>
+
+-- let g:NERDTreeDirArrowExpandable="+"
+-- let g:NERDTreeDirArrowCollapsible="~"
