255b637d7cdde8cf3713a9ce28b6a5b610f248a9
Michael Eberhardt <github@michael-eberhardt.de>
2023-03-11 11:24:24 +0200

    nerd fonts, icons and lot of comments to check out

--- init.lua
+++ init.lua
@@ -7,6 +7,9 @@ vim.cmd('set shiftwidth=4')
 vim.cmd('set smarttab')
 vim.cmd('set softtabstop=4')
 vim.cmd('set mouse=a')
+vim.cmd('set hlsearch')
+vim.cmd('set expandtab')
+-- check out: incsearch, mouse=v, showmatch, nocompatible, autoindent
 
 
 -- Key Remappings -------------------------------------------------------------
@@ -20,20 +23,40 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 local Plug = vim.fn['plug#']
 vim.call('plug#begin') -- call plug#begin()
 
-	-- more to be added here
-	Plug('https://github.com/vim-airline/vim-airline') -- Airline
-	Plug('https://github.com/preservim/nerdtree') -- NerdTree
-	Plug('https://github.com/nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'}) -- Tree-sitter
-
+    -- more to be added here
+    Plug('https://github.com/vim-airline/vim-airline') -- Airline (bottom info bar)
+    Plug('https://github.com/preservim/nerdtree') -- NerdTree (file tree browser)
+    Plug('https://github.com/ryanoasis/vim-devicons') -- VimDevIcons (beautiful icons)
+    -- Plug('vwxyutarooo/nerdtree-devicons-syntax')
+    Plug('https://github.com/nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'}) -- Tree-sitter
+    -- Plug('https://github.com/tpope/vim-surround') -- Surround (parentheses, brackets, quotes, XML tags)
+    -- Plug('https://github.com/tpope/vim-commentary') -- Commentary (comment & uncomment code)
+    -- Plug('https://github.com/neoclide/coc.nvim') -- Conquer of Completion (code completion)
+    -- Plug('preservim/nerdcommenter')
+    -- Plug('mhinz/vim-startify')
+    -- Plug 'dracula/vim'
+    -- Plug 'ryanoasis/vim-devicons'
+    -- Plug 'SirVer/ultisnips'
+    -- Plug 'honza/vim-snippets'
+    -- Plug 'https://github.com/preservim/tagbar' " Tagbar for code navigation
+    -- Plug 'https://github.com/terryma/vim-multiple-cursors' " CTRL + N for multiple cursors
+    
 vim.call('plug#end') -- call plug#end()
 
+vim.cmd('set encoding=UTF-8')
+vim.opt.guifont = { "Hack Nerd Font", "h12" }
 
 -- NerdTree Configuration -----------------------------------------------------
 
--- nnoremap <C-f> :NERDTreeFocus<CR>
+vim.cmd('let g:airline_powerline_fonts = 1')
+
 vim.cmd('nnoremap <C-n> :NERDTree<CR>')
 vim.cmd('nnoremap <C-t> :NERDTreeToggle<CR>')
-
+-- inoremap: maps the key in insert mode
+-- nnoremap: maps the key in normal mode
+-- vnoremap: maps the key in visual mode
+-- <C> : represents Control key
+-- <A> : Alt key
 
 
 -- Additional Configuration Scripts in './Lua' --------------------------------
