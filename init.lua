29d609615e0ba18a86063ac47ce5b4972d40ebb8
Michael Eberhardt <github@michael-eberhardt.de>
2023-05-18 16:42:54 +0300

    more cleanup

--- init.lua
+++ init.lua
@@ -31,19 +31,31 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin') -- call plug#begin()
 
     -- Main Plugins -------------------------------------------------
-    Plug('tpope/vim-fugitive') -- Some git-plugin for Airline...
-    Plug('nvim-lualine/lualine.nvim') -- LuaLine
-    Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'}) -- Tree-sitter
     Plug('nvim-tree/nvim-tree.lua') -- NVim-Tree, file browser
     Plug('nvim-tree/nvim-web-devicons') -- optional, for file icons
+    Plug('nvim-lualine/lualine.nvim') -- LuaLine
+    Plug('nvim-telescope/telescope.nvim', {['branch'] = '0.1.x'}) -- Telescope
+    Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'}) -- Tree-sitter
+    Plug('hrsh7th/nvim-cmp') -- Nvim CMP (Completion with LspConfig
+
+    -- Additional Plugins to extend nvim-cmp ------------------------
+    Plug('neovim/nvim-lspconfig') -- LPSConfig
+    Plug('hrsh7th/cmp-nvim-lsp')
+    Plug('hrsh7th/cmp-buffer')
+    Plug('hrsh7th/cmp-path')
+    Plug('hrsh7th/cmp-cmdline')
+    -- For luasnip users.
+    Plug('L3MON4D3/LuaSnip', {['tag'] = 'v1.2.1', ['do'] = 'make install_jsregexp'})
+    Plug('saadparwaiz1/cmp_luasnip')
+    -- TODO: Check friendly-snippet
+    -- Plug("rafamadriz/friendly-snippets")
+
+    -- Experimental Plugins /  To be checked ...
     Plug('marko-cerovac/material.nvim') -- Colorscheme material
     Plug('lukas-reineke/indent-blankline.nvim') -- Indent Blankline
     Plug('tpope/vim-commentary') -- Commentary (comment & uncomment code)
     -- TODO: Check Plug('numToStr/Comment')
     Plug('nvim-lua/plenary.nvim') -- Needed by Telescope
-    Plug('nvim-telescope/telescope.nvim', {['branch'] = '0.1.x'}) -- Telescope
-    Plug('neovim/nvim-lspconfig') -- LPSConfig
-    Plug('hrsh7th/nvim-cmp') -- Nvim CMP (Completion with LspConfig
     -- TODO: Also check out the following
     -- Plug('windwp/nvim-autopairs') -- Autopairs ?!
     -- Plug('mg979/vim-visual-multi') -- Multiple cursors, must see!
@@ -55,31 +67,6 @@ vim.call('plug#begin') -- call plug#begin()
     -- Plug 'preservim/tagbar' -- Tagbar for code navigation
     -- Plug 'terryma/vim-multiple-cursors' -- CTRL + N for multiple cursors
 
-    -- Additional Plugins to extend nvim-cmp ------------------------
-    -- Plug('neovim/nvim-lspconfig')
-    Plug('hrsh7th/cmp-nvim-lsp')
-    Plug('hrsh7th/cmp-buffer')
-    Plug('hrsh7th/cmp-path')
-    Plug('hrsh7th/cmp-cmdline')
-
-    -- For vsnip users.
-    -- Plug 'hrsh7th/cmp-vsnip'
-    -- Plug 'hrsh7th/vim-vsnip'
-
-    -- For luasnip users.
-    Plug('L3MON4D3/LuaSnip', {['tag'] = 'v1.2.1', ['do'] = 'make install_jsregexp'})
-    Plug('saadparwaiz1/cmp_luasnip')
-    -- TODO: Check friendly-snippet
-    -- Plug("rafamadriz/friendly-snippets")
-
-    -- For ultisnips users.
-    -- Plug 'SirVer/ultisnips'
-    -- Plug 'quangnguyen30192/cmp-nvim-ultisnips'
-
-    -- For snippy users.
-    -- Plug 'dcampos/nvim-snippy'
-    -- Plug 'dcampos/cmp-snippy'
-    
 vim.call('plug#end') -- call plug#end()
 
 
