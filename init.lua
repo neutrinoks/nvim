689db49df5f66510db300839526a65e52c854b2f
Michael Eberhardt <github@michael-eberhardt.de>
2023-05-18 16:37:30 +0300

    massive cleanup of files and structuring, replaced airline by lualine

--- init.lua
+++ init.lua
@@ -1,4 +1,28 @@
--- NVIM CONFIGURATION
+-- NEOVIM CONFIGURATION
+
+-- Notes ----------------------------------------------------------------------
+
+-- Note: vim.cmd('set bla') == opt.bla = 
+
+-- for longer embedded vim-code:
+-- vim.cmd [[
+--      vim code goes here
+-- ]]
+
+-- inoremap: maps the key in insert mode / i in vim.keymap.set()
+-- nnoremap: maps the key in normal mode / i in vim.keymap.set()
+-- vnoremap: maps the key in visual mode / i in vim.keymap.set()
+-- <C> : represents Control key
+-- <A> : Alt key
+-- <ESC> : Escape key
+
+-- Now we do start and define some shortcuts:
+local opt = vim.opt
+local g = vim.g
+local keymap = vim.keymap
+
+g.mapleader = ' '
+
 
 -- Plugin-Manager / Plugins ---------------------------------------------------
 
@@ -8,7 +32,7 @@ vim.call('plug#begin') -- call plug#begin()
 
     -- Main Plugins -------------------------------------------------
     Plug('tpope/vim-fugitive') -- Some git-plugin for Airline...
-    Plug('vim-airline/vim-airline') -- Airline (bottom info bar)
+    Plug('nvim-lualine/lualine.nvim') -- LuaLine
     Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'}) -- Tree-sitter
     Plug('nvim-tree/nvim-tree.lua') -- NVim-Tree, file browser
     Plug('nvim-tree/nvim-web-devicons') -- optional, for file icons
@@ -58,39 +82,47 @@ vim.call('plug#begin') -- call plug#begin()
     
 vim.call('plug#end') -- call plug#end()
 
-vim.cmd('set encoding=UTF-8')
-vim.opt.guifont = { "Hack Nerd Font", "h13" }
-
 
 -- General Editor Settings ----------------------------------------------------
 
-vim.cmd('set number')
-vim.cmd('set relativenumber')
-vim.cmd('set autoindent')
-vim.cmd('set tabstop=4')
-vim.cmd('set shiftwidth=4')
-vim.cmd('set smarttab')
-vim.cmd('set softtabstop=4')
-vim.cmd('set mouse=a')
-vim.cmd('set hlsearch')
-vim.cmd('set expandtab')
-vim.cmd('set scrolloff=10')
-vim.cmd('set cc=100')
-vim.cmd('set showmatch')
--- check out: incsearch, mouse=v, nocompatible 
+opt.encoding = 'UTF-8'
+opt.guifont = "Hack Nerd Font" -- , "H12" 
+opt.number = true
+opt.relativenumber = true
+opt.autoindent = true
+opt.tabstop = 4
+opt.smarttab = true
+opt.softtabstop = 4
+opt.hlsearch = true
+opt.mouse = a
+opt.expandtab = true
+opt.scrolloff = 10
+opt.colorcolumn = "100"
+opt.showmatch = true
+opt.cursorline = false
+opt.showmode = false
+-- opt.laststatus = 3
+opt.incsearch = true
+
+
+-- Additional Configuration Scripts in './Lua' --------------------------------
+
+require('cfg_nvimtree')
+require('cfg_treesitter')
+require('cfg_colorscheme')
+require('cfg_blankline')
+-- require('cfg_airline')
+-- require('cfg_telescope')
+require('cfg_lspconfig')
+require('cfg_cmp')
+require('cfg_lualine')
 
 
 -- Key Remappings -------------------------------------------------------------
 
--- inoremap: maps the key in insert mode / i in vim.keymap.set()
--- nnoremap: maps the key in normal mode / i in vim.keymap.set()
--- vnoremap: maps the key in visual mode / i in vim.keymap.set()
--- <C> : represents Control key
--- <A> : Alt key
--- <ESC> : Escape key
+-- Open Ex via '<space> c v'
+keymap.set({'n','i','v'}, '<leader>cv', 'Ex')
 
--- define a global mapleader: <spacebar>
-vim.g.mapleader = ' '
 -- exit insert mode, return to normal mode, by jj
 vim.keymap.set('i', 'jj', '<ESC>')
 
@@ -99,19 +131,4 @@ vim.keymap.set('i', 'jj', '<ESC>')
 --nnoremap <C-Right> :tabnext<CR>
 --nnoremap <C-Left> :tabprevious<CR>
 
--- Additional Configuration Scripts in './Lua' --------------------------------
 
-require('nvimtree')
-require('treesitter')
-require('colorscheme')
-require('blankline')
-require('airline')
-require('telescope')
-require('mylspconfig')
-require('mycmp')
--- require('coc')
-
--- for longer embedded vim-code:
--- vim.cmd [[
---      vim code goes here
--- ]]
