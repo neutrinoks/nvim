95620b31011a69819443de269b54f73da8ca2461
Michael Eberhardt <64731211+neutrinoks@users.noreply.github.com>
2024-06-24 15:00:28 +0400

    feat: updated keymapping style, US layout oriented, common use PC and Mac

--- init.lua
+++ init.lua
@@ -135,14 +135,14 @@ require('cfg_colorscheme')
 local opts = { noremap = true, silent = true }
 
 -- Exit insert mode, return to normal mode, by jj
-keymap.set('i', 'jj', '<ESC>', opts)
+keymap.set('i', 'nn', '<ESC>', opts)
 -- Save file by: Ctrl-s
-keymap.set({'n','i'}, '<C-s>', vim.cmd.w, opts)
+keymap.set({'n','i'}, '<leader>ss', vim.cmd.w, opts)
 -- Pseudo-quit, aka close by: Alt-q
-keymap.set('n', '<A-q>', vim.cmd.q, opts)
+keymap.set('n', '<leader>qq', vim.cmd.q, opts)
 -- Setup bright daylight or darg night colorscheme
-keymap.set('n', '<C-A-l>', '<Cmd>Light<CR>', opts)
-keymap.set('n', '<C-A-d>', '<Cmd>Dark<CR>', opts)
+keymap.set('n', '<leader>ll', '<Cmd>Light<CR>', opts)
+keymap.set('n', '<leader>dd', '<Cmd>Dark<CR>', opts)
 
 -- NvimTree Keymapping
 keymap.set('n', '<C-n>', vim.cmd.NvimTreeToggle, opts)
@@ -167,10 +167,10 @@ keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- -> in cfg_cmp.lua
 
 -- Barbar Keymapping
-keymap.set({'n','i'}, '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-keymap.set({'n','i'}, '<A-.>', '<Cmd>BufferNext<CR>', opts)
-keymap.set('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-keymap.set('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+keymap.set({'n','i'}, '<C-,>', '<Cmd>BufferPrevious<CR>', opts)
+keymap.set({'n','i'}, '<C-.>', '<Cmd>BufferNext<CR>', opts)
+keymap.set('n', '<C-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+keymap.set('n', '<C->>', '<Cmd>BufferMoveNext<CR>', opts)
 keymap.set('n', '<C-w>', '<Cmd>BufferClose<CR>', opts)
 
 -- Setup colorscheme in dependency of current daytime --------------------------
